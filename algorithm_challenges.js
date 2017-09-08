a4b47a2640a44b2d7213d0a712505fa621c23df8
Kevin E. Post <kevinpost16@gmail.com>
2017-09-07 22:28:54 -0400

    Update algorithm_challenges.js

--- algorithm_challenges.js
+++ algorithm_challenges.js
@@ -140,6 +140,25 @@ function FirstFactorial(num) {
 }
    
 FirstFactorial(4); 
+/////////////////////////////////////////////
+function FirstFactorial(num) { 
+
+  // our factorial function
+  function factorial(n) { 
+ 
+    // terminate the recursion once we hit zero
+    if (n===0) { return 1; }
+
+    // otherwise keep calling the function recursively 
+    else { return factorial(n-1) * n; }
+
+  }
+
+  return factorial(num);
+         
+}
+   
+FirstFactorial(4); 
 
 /////////////////////////////////palidrome
 function myFun(nums){
